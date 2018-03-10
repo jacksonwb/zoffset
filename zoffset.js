533d530c4f856f78fe82c32436e4cb49dace4e4b
jacksonwb <jaywild@gmail.com>
2018-03-10 14:06:33 -0700

    Add parentheses exclusion, Z >= 20 exclusion, correct preceding space handling

--- zoffset.js
+++ zoffset.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 //read a text file syncronously into the data variable
 try {
-	var data = fs.readFileSync('LU15 Z TEST.txt', 'utf8');
+	var data = fs.readFileSync('fulltest.txt', 'utf8');
 	//console.log(data);
 } catch(e) {
 	console.log('Error:', e.stack);
@@ -15,36 +15,45 @@ let newArray = workingArray;
 //console.log(workingArray);
 
 //Processing Logic 
-//Set Match String to find Z Values
-//Current match criteria - Match a Z number that is preceded by a space, 
+//Set Match String to find Parentheses expressions and Z Values
+//Parentheses expressions are returned by the replacer unaltered, 
+//numbers following valid Z values are captured
+//Current match criteria - Match a Z that is not followed by an = sign
 //up to the next space,new line, or string end
 
-let matchString = /\sZ(.*?)(?=$|\s)/g;
+let matchString = /\(.*\)|Z(?!=)(.*?)(?=$|\s)/g;
 
-//Replacer Function - Finds Z values, converst to num, offsets, converts back to string
-function zOffset (match,numberPart) {
+//Replacer Function - Skips () statements, Finds Z values, converst to num, offsets, converts back to string
+function zOffset (match, numberPart) {
 	//console.log(match);
 	//console.log('Number part is: ' + numberPart);
-
-	let zValue = Number(numberPart);
-	//console.log(zValue);
-
-	//Add zValue Offset - Multiply and divide by 10000 to eliminate floating point errors
-	let zOffsetValue = 5;
-	zValue = (zValue*10000 + zOffsetValue*10000)/10000;
-	//console.log(zValue);
-
-	//If the number is an integer, ensure a decimal is included in the string conversion
-	if (Number.isInteger(zValue)) {
-		var newString = ' Z' + zValue.toFixed(1); //Preceding space is added back into the string
+	if (typeof numberPart == 'undefined'){
+		return match;
 	} else {
-		var newString = ' Z' + zValue.toString();
-	}
+		let zValue = Number(numberPart);
+		//console.log(zValue);
 
-	//console.log(newString);
-	return newString;
+		if (zValue >= 20) {
+			return match;
+		}
+		//Add zValue Offset - Multiply and divide by 10000 to eliminate floating point errors
+		let zOffsetValue = 5;
+		zValue = (zValue*10000 + zOffsetValue*10000)/10000;
+		//console.log(zValue);
+
+		//If the number is an integer, ensure a decimal is included in the string conversion
+		if (Number.isInteger(zValue)) {
+			var newString = 'Z' + zValue.toFixed(1); //Preceding space is added back into the string
+		} else {
+			var newString = 'Z' + zValue.toString();
+		}
+		
+		return newString;
+	}
+	
 }
 
+//Moves through entire document array
 for (let i = 0; i <= workingArray.length - 1 ; i++) {
 	//console.log(i);
 	console.log('Working array is: ' + workingArray[i]);
